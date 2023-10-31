0d26efb300c19b1ec147e4e192b89e11957c7008
Daniel R. Cuesta-Aguirre <97441691+DanielRCA@users.noreply.github.com>
2023-10-31 10:15:47 +0100

    Update NCR_ampliconbasedngs.sh
    Corrections

--- NCR_ampliconbasedngs.sh
+++ NCR_ampliconbasedngs.sh
@@ -3,7 +3,9 @@
 ## time bash NCR_ampliconbasedngs.sh 2>&1 | tee NCR_ampliconbasedngs.log
 
 
-source activate ngs_analysis
+source activate env_ncrngsamplicons
+
+
 main_dir=$(pwd)
 
 #A directory to save the files generated is made
@@ -16,11 +18,12 @@ fi
 
 #A directory to save the reference is made
 ref=reference
-seq_ref=rCRS_NCR_linear.fasta
+seq_ref=rCRS_NCR_lineal.fasta
+
 
 if [[ -d ${ref} ]]; then
     echo "Directory ${ref} already exist, it is not going to be created again"
-    NCR=$(pwd)/${ref}/${seq_ref}
+    NCR=${main_dir}/${ref}/${seq_ref}
 else
     echo "A directory is created to keep the reference sequence and its indexes"
     mkdir ${ref}
@@ -32,24 +35,29 @@ if [[ -e ${seq_ref}.amb ]] && [[ -e ${seq_ref}.fai ]] ; then
     echo "
     Indexes were already created
     "
-    picard CreateSequenceDictionary R=${seq_ref} O=rCRS_NCR_linear.dict
 else
     echo "
     Indexes are being created
     "
     time bwa index ${seq_ref}
     time samtools faidx ${seq_ref}
-    picard CreateSequenceDictionary R=${seq_ref} O=rCRS_NCR_linear.dict
+    picard CreateSequenceDictionary R=${seq_ref} O=rCRS_NCR_lineal.dict
 fi
 cd ${main_dir}
 
 NCR=${ref}/${seq_ref}
 
-#A list with all the directories that have samples is made
-for sample in *; do if  [[ -d ${d} ]] && [[ ${d} != ${vis} ]] && [[ ${d} != ${ref} ]]; then  echo $sample; fi done > samples.txt
 
-echo -e "Sample\tPosition\tAlternative_base\tN_total\tN_alternative\tPercentage" > base_mix_cov5.txt
-echo -e "Sample\tPosition\tAlternative_base\tN_total\tN_alternative\tPercentage" > base_mix_cov10.txt
+
+cd ${main_dir}
+
+NCR=${main_dir}/${ref}/${seq_ref}
+
+#A list with all the directories that have samples is made
+for samples in *; do if  [[ -d ${samples} ]] && [[ ${samples} != ${vis} ]] && [[ ${samples} != ${ref} ]]; then  echo ${samples}; fi done > samples.txt
+
+echo -e "Sample\tPosition\tAlternative_base\tN_total\tN_alternative\tPercentage" > base_mix_cov_5.txt
+echo -e "Sample\tPosition\tAlternative_base\tN_total\tN_alternative\tPercentage" > base_mix_cov_10.txt
 
 while read sample; do
       
@@ -86,8 +94,9 @@ while read sample; do
     echo "${sample} ${num_1}" >> ${main_dir}/tem_all.txt
     
     #Percentage of duplication
-    dup_rate=$(echo "$(grep duplication -m 1 -A1 ${sample}_temp.json | grep rate | sed "s/.*://g" | sed "s/,//g")*100" | bc)
-	echo "${sample} ${dup_rate}" >> ${file}/tem_dup_rate.txt
+    dup_rate=$(echo "$(grep duplication -m 1 -A1 ${sample}_temp.json | grep rate | sed "s/.*://g" | sed "s/,//g")*100" | bc)    
+    echo "${sample} ${dup_rate}" >> ${main_dir}/tem_dup_rate.txt
+
     
     #Second fastp. To trim primers (25pb from 5') and 5pb in 3'
 	fastp --trim_front1 25 --trim_tail1 5 \
@@ -119,16 +128,18 @@ while read sample; do
         mkdir ${fastq_file}
         fastqc -o ${fastq_file} -f fastq *.fq.gz
     fi
-        
+    
+    
     #Alignment is done with bwa aln and bwa samse/sampe   
     bwa aln -t 9 -l 1024 -n 0.01 -o 2 ${NCR} ${sample}_R1.fq.gz > tem_aln_R1.sai
     bwa aln -t 9 -l 1024 -n 0.01 -o 2 ${NCR} ${sample}_R2.fq.gz > tem_aln_R2.sai
     bwa aln -t 9 -l 1024 -n 0.01 -o 2 ${NCR} ${sample}_M.fq.gz > tem_aln_M.sai
     
+    
     bwa sampe ${NCR} tem_aln_R1.sai tem_aln_R2.sai ${sample}_R1.fq.gz ${sample}_R2.fq.gz | samtools view -Sbh -F4 -q30 | samtools sort -O bam -o tem_${sample}_NM_map.bam
     bwa samse ${NCR} tem_aln_M.sai ${sample}_M.fq.gz | samtools view -Sbh -F4 -q30 | samtools sort -O bam -o tem_${sample}_M_map.bam
 
-    samtools merge -o tem_${sample}_map_unsort.bam tem_${sample}_NM_map.bam tem_${sample}_M_map.bam; samtools sort -O bam -o final_${Sample}.bam tem_${sample}_map_unsort.bam
+    samtools merge -o tem_${sample}_map_unsort.bam tem_${sample}_NM_map.bam tem_${sample}_M_map.bam; samtools sort -O bam -o final_${sample}.bam tem_${sample}_map_unsort.bam
     bam=final_${sample}.bam
     samtools index ${bam}
         
@@ -143,7 +154,7 @@ while read sample; do
         
     #The number of reads with PMD is determined
     num_3=$(samtools view -c ${pam})
-    echo "${sample} ${num_3}" >> ${main_dir}/tem_no_dups_utils_pmd.txt
+    echo "${sample} ${num_3}" >> ${main_dir}/tem_useful_pmd.txt
 
     #Quality and mean depth coverage of bams is checked for the file with all reads
 	qualimap bamqc -bam ${bam} -c -gd hg19 -outdir 3_qualimap_bamqc
@@ -174,8 +185,10 @@ while read sample; do
 	echo "${sample}_10_pmd ${range_pmd}" >> ${main_dir}/tem_range_pmd_10.txt
     rm temp_pmd.txt
     
+    bases_with_good_cov=$(samtools depth ${bam} | awk '$3>4 {print $2"\t"1}' | awk '{sum += $2} END {print sum}')
+    echo -e "${sample}\t${bases_with_good_cov}" >> ${main_dir}/tem_goodcovinsamples_5.txt
     bases_with_good_cov=$(samtools depth ${bam} | awk '$3>9 {print $2"\t"1}' | awk '{sum += $2} END {print sum}')
-    echo -e "${sample}\t${bases_with_good_cov}" >> ${main_dir}/tem_goodcovinsamples.txt
+    echo -e "${sample}\t${bases_with_good_cov}" >> ${main_dir}/tem_goodcovinsamples_10.txt
 
     #Variant call is done with freebayes and vcfallelicprimitives
     echo "
@@ -195,13 +208,13 @@ while read sample; do
 	awk '$3 == "." && length($5) == 1 && length($4) == 1 {print $2"\t"$4"\t"$5"\t"$10}' ${pam}_10.vcf | sed 's/:/\t/g' | awk '{print $1"\t"$2"\t"$3"\t"$5"\t"$6"\t"100*$9/$5}' | awk '{if ($6 < 70) print $1"\t"$2$3; else print $1"\t"$3;}' | sed 's/[AGTC]\{4\}/N/g' | sed 's/[ACG]\{3\}/V/g' |sed 's/[ATC]\{3\}/H/g' | sed 's/[ATG]\{3\}/D/g' | sed 's/[GTC]\{3\}/B/g' | sed 's/[AC]\{2\}/M/g' | sed 's/[TG]\{2\}/K/g' | sed 's/[AT]\{2\}/W/g' | sed 's/[GC]\{2\}/S/g' | sed 's/[TC]\{2\}/Y/g' | sed 's/[AG]\{2\}/R/g' | awk '{if ($1<670) print $1+15900$2; else print $1-669$2}' | sed -e 's/\t$//g' | awk -F"\t" '$1 < 303 || $1 > 315 {print $1$2}' > ${pam}_10.txt
 		
 	#Documents with mixtures are done for all reads
- 	awk '$3 == "." && length($4) == 1 && length($5) == 1 {if ($2<670) print $2+15900"\t"$5"\t"$10; else print $2-669"\t"$5"\t"$10}' ${bam}_5.vcf | awk  '$1 < 303 || $1 > 315 {print $0}' | sed 's/:/\t/g' | awk '{print $1"\t"$2"\t"$4"\t"$8"\t"100*$8/$4"\t"}' | awk -v vd=$d '$5 < 70 {print vd"\t"$0}' >> ${main_dir}/base_mix_5.txt
+ 	awk '$3 == "." && length($4) == 1 && length($5) == 1 {if ($2<670) print $2+15900"\t"$5"\t"$10; else print $2-669"\t"$5"\t"$10}' ${bam}_5.vcf | awk  '$1 < 303 || $1 > 315 {print $0}' | sed 's/:/\t/g' | awk '{print $1"\t"$2"\t"$4"\t"$8"\t"100*$8/$4"\t"}' | awk -v vd=${sample} '$5 < 70 {print vd"\t"$0}' >> ${main_dir}/base_mix_cov_5.txt
 	
-	awk '$3 == "." && length($4) == 1 && length($5) == 1 {if ($2<670) print $2+15900"\t"$5"\t"$10; else print $2-669"\t"$5"\t"$10}' ${bam}_10.vcf | awk  '$1 < 303 || $1 > 315 {print $0}' | sed 's/:/\t/g' | awk '{print $1"\t"$2"\t"$4"\t"$8"\t"100*$8/$4"\t"}' | awk -v vd=$d '$5 < 70 {print vd"\t"$0}' >> ${main_dir}/base_mix_10.txt
+	awk '$3 == "." && length($4) == 1 && length($5) == 1 {if ($2<670) print $2+15900"\t"$5"\t"$10; else print $2-669"\t"$5"\t"$10}' ${bam}_10.vcf | awk  '$1 < 303 || $1 > 315 {print $0}' | sed 's/:/\t/g' | awk '{print $1"\t"$2"\t"$4"\t"$8"\t"100*$8/$4"\t"}' | awk -v vd=${sample} '$5 < 70 {print vd"\t"$0}' >> ${main_dir}/base_mix_cov_10.txt
  
     
-    cp ${bam}.txt ${main_dir}/${vis}
-    cp ${pam}.txt ${main_dir}/${vis}
+    cp ${bam}_*.txt ${main_dir}/${vis}
+    cp ${pam}_*.txt ${main_dir}/${vis}
     
     rm *tem*
     echo "
@@ -220,7 +233,7 @@ echo "Sample Range Haplogroup Haplotype" > tem_haplogrep_format.txt
 #Variable with the same value for all samples is generated
 haplogroup="?"
 #The rest of the variables are generated line by line
-for t in *final*bam.txt; do
+for t in *final*bam_*.txt; do
     #Name is extracted
     sample="${t//*final_/}"
     if [[ ${t} == pmd* ]]
@@ -238,7 +251,7 @@ for t in *final*bam.txt; do
 			ranges=$(awk -v SAMPLE=${sample} '$1==SAMPLE {print $2}' ${main_dir}/tem_range_5.txt)
 		else
 			sample="${sample//.bam_10.*/_10}"
-			ranges=$(awk -v SAMPLE=${sample} '$1==SAMPLE {print $2}' ${mamin_dir}/tem_range_10.txt)
+			ranges=$(awk -v SAMPLE=${sample} '$1==SAMPLE {print $2}' ${main_dir}/tem_range_10.txt)
 		fi
     fi
     #Fake tabulators are converted into real ones
@@ -267,7 +280,7 @@ bash haplogrep classify --in tem_haplogrep_format.txt --format hsd --out tem_hap
 echo "
 Generating final table...
 "
-echo -e 'ID\tTotal_reads\tDuplication_rate\tUtil_reads\tPercentage_of_util_reads\tMean_depth_coverage\tMean_mapping_quality\tDamaged_util_reads\tPercentage_of_Damaged_reads\tHeteroplamies_cov5\tHaplogrouop_cov5\tQuality_cov5\tPercentage_of_NCR_recovered_cov5\tRange_cov5\tHaplotype_cov5\tHeteroplamies_cov10\tHaplogrouop_cov10\tQuality_cov10\tPercentage_of_NCR_recovered_cov10\tRange_cov10\tHaplotype_cov10' > final_table.txt
+echo -e 'ID\tTotal_reads\tDuplication_rate\tUtil_reads\tPercentage_of_util_reads\tMean_depth_coverage\tMean_mapping_quality\tDamaged_util_reads\tPercentage_of_Damaged_reads\tMixed_bases_cov5\tHaplogrouop_cov5\tQuality_cov5\tPercentage_of_NCR_recovered_cov5\tRange_cov5\tHaplotype_cov5\tMixed_bases_cov10\tHaplogrouop_cov10\tQuality_cov10\tPercentage_of_NCR_recovered_cov10\tRange_cov10\tHaplotype_cov10' > final_table.txt
 
 while read sample; do
 	echo "${sample}" | awk -v v0=${sample} \
@@ -277,12 +290,12 @@ while read sample; do
 	-v v4=$(awk -v v4_1=${sample} '$1==v4_1 {print $2}' tem_cov.txt) \
 	-v v5=$(awk -v v5_1=${sample} '$1==v5_1 {print $2}' tem_qual.txt) \
 	-v v6=$(awk -v v6_1=${sample} '$1==v6_1 {print $2}' tem_useful_pmd.txt) \
-	-v v7=$(awk -v v7_1=${sample} '$1==v7_1 {print $0}' base_mix_5.txt | wc -l) \
+	-v v7=$(awk -v v7_1=${sample} '$1==v7_1 {print $0}' base_mix_cov_5.txt | wc -l) \
 	-v v8=$(awk -v v8_1=\"${sample}_5\" '$1==v8_1 {print $2"\t"$4}' tem_haplos.txt | sed 's/"//g' | sed 's/\t/--/g') \
 	-v v9=$(awk -v v9_1=${sample} '$1==v9_1 {print $2}' tem_goodcovinsamples_5.txt) \
 	-v v10=$(awk -v v10_1=${sample}_5 '$1==v10_1 {print $2}' tem_range_5.txt) \
 	-v v11=$(awk -v v11_1=${sample}_5 '$1==v11_1 {print $0}' tem_haplogrep_format.txt | cut -f 4- | sed 's/\t/__/g') \
-	-v v12=$(awk -v v12_1=${sample} '$1==v12_1 {print $0}' base_mix_10.txt | wc -l) \
+	-v v12=$(awk -v v12_1=${sample} '$1==v12_1 {print $0}' base_mix_cov_10.txt | wc -l) \
 	-v v13=$(awk -v v13_1=\"${sample}_10\" '$1==v13_1 {print $2"\t"$4}' tem_haplos.txt | sed 's/"//g' | sed 's/\t/--/g') \
 	-v v14=$(awk -v v14_1=${sample} '$1==v14_1 {print $2}' tem_goodcovinsamples_10.txt) \
 	-v v15=$(awk -v v15_1=${sample}_10 '$1==v15_1 {print $2}' tem_range_10.txt) \
